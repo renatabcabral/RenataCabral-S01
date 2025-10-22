730f8b1cbfcf020ad663c065a6cd75ac99ef0876
Renata Cabral <162451050+renatabcabral@users.noreply.github.com>
2025-10-22 09:57:01 -0300

    Implement Drink and DrinkLenda classes in Ruby

---
+++ Exercicio 09, POO, Ruby - bar.rb
@@ -0,0 +1,56 @@
+class Drink
+  attr_reader :nome
+
+  def initialize(nome, preco_base)
+    @nome = nome
+    self.preco_base = preco_base
+  end
+
+  def preco_base
+    @preco_base
+  end
+
+  def preco_base=(valor)
+    if valor > 0
+      @preco_base = valor
+    else
+      puts "O preço deve ser positivo!"
+      @preco_base = 0
+    end
+  end
+
+  def to_s
+    "Drink: #{@nome}, Preço base: $#{@preco_base}"
+  end
+end
+
+class DrinkLenda < Drink
+  def initialize(nome, preco_base, anos_desde_criacao)
+    super(nome, preco_base)
+    @anos_desde_criacao = anos_desde_criacao
+  end
+
+  def preco_final
+    preco_base + (5 * @anos_desde_criacao)
+  end
+
+  def to_s
+    "Drink Lenda: #{@nome}, Preço base: $#{@preco_base}, " \
+    "Anos desde criação: #{@anos_desde_criacao}, " \
+    "Preço final: $#{preco_final}"
+  end
+end
+
+puts "Digite o nome do drink:"
+nome = gets.chomp
+
+puts "Digite o preço base do drink:"
+preco_base = gets.chomp.to_f
+
+puts "Digite a idade do drink em anos:"
+anos = gets.chomp.to_i
+
+drink = DrinkLenda.new(nome, preco_base, anos)
+
+puts "\n--- Resultado ---"
+puts drink.to_s
